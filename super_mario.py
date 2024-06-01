669b7958007b295adfb3a6b373e14b5719be8cff
gulsen sesli ozturk <gulsensesliozturk@hotmail.com>
2024-06-01 12:45:16 +0300

    proje kalsoru oluşturuldu

---
+++ super_mario.py
@@ -0,0 +1,12 @@
+"""
+super marıo merdivenleri için kodlama
+
+Kodunuzu Nasıl Test Edebilirsiniz? Kodunuzu girdiğinizde belirtildiği gibi çalışıyor mu?
+** -1 (veya diğer negatif sayıların girişi hatalıdır)?
+** 0 girişi hatalıdır?
+** 1'den 8'e kadar sayılara müsaade edilir?
+** 9 veya 9'dan büyük başka pozitif sayıların girişi hatalıdır?
+** harf veya kelime girişi hatalıdır?
+** yalnızca Enter'a basılması yani hiç giriş olmaması hatalıdır.?
+
+"""
